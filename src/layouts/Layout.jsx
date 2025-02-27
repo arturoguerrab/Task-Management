545ae50a919dde4bd7a1928bdbb347ac9a4452dc
Arturo Guerra <arturoguerrab12@gmail.com>
2025-02-27 16:44:58 -0300

    Creacion del dashboard principal

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { PageContainer } from '@toolpad/core/PageContainer';
+
+const Layout = () => {
+  return (
+    <DashboardLayout>
+      <PageContainer>
+        <Outlet />
+      </PageContainer>
+    </DashboardLayout>
+  )
+}
+
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./layouts/Layout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Stores from "./pages/Stores.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    Component: App, // root layout route
+    children: [
+      {
+        path: '/',
+        Component: Layout,
+        children: [
+          {
+            path: '',
+            Component: Dashboard,
+          },
+          {
+            path: 'stores',
+            Component: Stores,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
